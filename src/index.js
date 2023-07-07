9802523e2c99c4144b30a2b0b3ee3dbe4c040b4a
JavohirDeveloper <muhammadiyevj72@gmail.com>
2023-07-07 20:46:57 +0500

    01_lesson crud

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { readFileSync } from "fs";
 
 // 01_lesson
-// import resolvers from './lesson/01_lesson.js';
-// const typeDefs = readFileSync('./src/schema/01_lesson_schema.gql', 'utf8');
+import resolvers from './lesson/01_lesson.js';
+const typeDefs = readFileSync('./src/schema/01_lesson_schema.gql', 'utf8');
 
 // 02_lesson
 // import resolvers from './lesson/02_lesson.js';
 // const typeDefs = readFileSync('./src/schema/02_lesson_schema.gql', 'utf8');
 
 // 03_lesson
-import resolvers from "./lesson/03_lesson.js";
-const typeDefs = readFileSync("./src/schema/03_lesson_schema.gql", "utf8");
+// import resolvers from "./lesson/03_lesson.js";
+// const typeDefs = readFileSync("./src/schema/03_lesson_schema.gql", "utf8");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-startStandaloneServer(server, { listen: { port: 8585 } }).then(({ url }) => {
+startStandaloneServer(server, { listen: { port: 8586 } }).then(({ url }) => {
   console.log(`Server is running on url: ${url}`);
 });
--- src/lesson/01_lesson.js
+++ src/lesson/01_lesson.js
@@ -54,8 +54,87 @@ const meals = [
 const resolvers = {
   Query: {
     meals: () => meals,
+    meal: (_, args) => {
+      const meal = meals.find((m) => m.id == args.id);
+      if (!meal) {
+        throw new Error("Meals not found");
+      }
+      return meal;
+    },
     categories: () => categories,
+    category: (_, args) => {
+      const category = categories.find((c) => c.id == args.id);
+      if (category) {
+        throw new Error("Category not found");
+      }
+    },
   },
+  Mutation: {
+    createMeal: (_, args) => {
+      meals.push({
+        id: meals.length + 1,
+        name: args.input.name,
+        price: args.input.price,
+        quantity: args.input.quantity
+      });
+      return meals.at(-1);
+    },
+    updateMeal: (_, args) => {
+      const meal = meals.find((m) => m.id == args.id);
+      const index = meals.findIndex((m) => m.id == args.id);
+      if (!meal) {
+        throw new Error("Meals not found");
+      }
+
+      meals.splice(index, 1, { ...meal, ...args.input });
+
+      return meals[index];
+    },
+    removeMeal: (_, args) => {
+      const meal = meals.find((c) => c.id == args.id);
+      const index = meals.findIndex((c) => c.id == args.id);
+
+      if (!meal) {
+        throw new Error("Category not found");
+      }
+
+      categories.splice(index, 1);
+
+      return meal;
+    },
+    createCategory: (_, args) => {
+      categories.push({
+        id: categories.length + 1,
+        name: args.input.name,
+      });
+      return categories.at(-1);
+    },
+    updateCategory: (_, args) => {
+      const category = categories.find((c) => c.id == args.id);
+      const index = categories.findIndex((c) => c.id == args.id);
+
+      if (!category) {
+        throw new Error("Category not found");
+      }
+
+      categories.splice(index, 1, { ...category, ...args.input });
+
+      return categories[index];
+    },
+    removeCategory: (_, args) => {
+      const category = categories.find((c) => c.id == args.id);
+      const index = categories.findIndex((c) => c.id == args.id);
+
+      if (!category) {
+        throw new Error("Category not found");
+      }
+
+      categories.splice(index, 1);
+
+      return category;
+    }
+  },
+
   Meal: {
     category: (parent) => {
       return categories.find((category) => category.id === parent.category_id);
